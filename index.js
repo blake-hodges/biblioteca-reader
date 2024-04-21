8a82a2d2dbdc41f15fab4c5abb3e0c1e19b57e13
Blake Hodges <tbhodges@gmail.com>
2024-04-21 12:31:48 -0400

    add sqlite database and connect to database

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
+
+let db = new sqlite3.Database('./biblioteca.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening database', err.message)
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+})
+
+// Execute a query
+db.serialize(() => {
+    db.each(`SELECT * FROM users`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(row); // Assuming your table has columns id and name
+    });
+});
+
+// Close the database
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+});
+
 
 
 const app = express()
