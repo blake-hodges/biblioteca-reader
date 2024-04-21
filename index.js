cfac0f1398963d7308748062a4f1d9dd73875ad0
Blake Hodges <tbhodges@gmail.com>
2024-04-21 15:06:14 -0400

    add api route to list all authors

--- index.js
+++ index.js
@@ -10,22 +10,16 @@ let db = new sqlite3.Database('./biblioteca.db', sqlite3.OPEN_READWRITE, (err) =
 })
 
 // Execute a query
-db.serialize(() => {
-    db.each(`SELECT * FROM users`, (err, row) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log(row); // Assuming your table has columns id and name
-    });
-});
+// db.serialize(() => {
+//     db.each(`SELECT * FROM authors`, (err, row) => {
+//         if (err) {
+//             console.error(err.message);
+//         }
+//         console.log(row); // Assuming your table has columns id and name
+//     });
+// });
 
-// Close the database
-db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Closed the database connection.');
-});
+
 
 
 
@@ -37,6 +31,25 @@ app.get("/", (req, res) => {
     res.send("hello world")
 })
 
+app.get("/api/authors", (req, res) => {
+    db.all("SELECT * FROM authors", [], (err, rows) => {
+        if (err) {
+            res.status(400).send(err.message);
+            return;
+        }
+        res.json(rows);
+    });
+})
+
 app.listen(3000, () => {
     console.log("server started on port 3000")
 })
+
+
+// // Close the database
+// db.close((err) => {
+//     if (err) {
+//         console.error(err.message);
+//     }
+//     console.log('Closed the database connection.');
+// });
