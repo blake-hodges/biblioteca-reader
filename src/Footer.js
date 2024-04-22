1495437a28c166ecf07c4bbd7287bd6451f9165d
Blake Hodges <tbhodges@gmail.com>
2024-04-22 18:07:52 -0400

    delete old html files

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-[#00df9a] w-full p-2 flex justify-center relative mt-2">
             <div>
-                <p className="text-white">Designed and Built by <a className="text-gray-500 underline" href="https://blakehodgesdev.com" target="_blank">Blake Hodges</a></p>
+                <p className="text-white">Built by <a className="text-gray-500 underline" href="https://blakehodgesdev.com" target="_blank">Blake Hodges</a></p>
             </div>
         </footer>
     )
