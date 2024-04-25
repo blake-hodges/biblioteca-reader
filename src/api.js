95da261599a6c073b6097a8fceae19f7e4f1fbf5
Blake Hodges <tbhodges@gmail.com>
2024-04-25 18:49:10 -0400

    add error handling to  listBooks function

--- src/api.js
+++ src/api.js
@@ -2,9 +2,18 @@
 
 
 const listBooks = async () => {
-    let response = await fetch("/api/books")
-    return await response.json()
+    try {
+        let response = await fetch("/api/books")
+        return await response.json()
+    } catch(err) {
+        console.log(err)
+    }
 }
+
+// const listAuthors = async () => {
+//     let response = await fetch("/api/authors")
+//     return await response.json()
+// }
 
 
 export { listBooks }
